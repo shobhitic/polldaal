cdbc16f275ad3220b9513c10385890ac8a9c3b22
Shobhit Bakliwal <shobhitic@gmail.com>
2012-11-16 00:29:25 +0530

    Added paddings and correct tags for proper display using CSS

--- app/helpers/choices_helper.rb
+++ app/helpers/choices_helper.rb
@@ -1,13 +1,13 @@
 module ChoicesHelper
-  def get_choices_based_on_user_votes(choice)
+  def get_choices_based_on_user_votes(choice, default_class)
     if @vote
       if @vote.choice == choice
-        return button_to choice.description, vote_path(@vote, :choice_id => choice), :class => "btn btn-inverse", :method => :put
+        return button_to choice.description, vote_path(@vote, :choice_id => choice), :class => "#{default_class} btn-inverse", :method => :put
       else
-        return button_to choice.description, vote_path(@vote, :choice_id => choice), :class => "btn", :method => :put
+        return button_to choice.description, vote_path(@vote, :choice_id => choice), :class => "#{default_class}", :method => :put
       end
     else
-      return button_to choice.description, votes_path(:choice_id => choice), :class => "btn"
+      return button_to choice.description, votes_path(:choice_id => choice), :class => "#{default_class}"
     end
   end
 
